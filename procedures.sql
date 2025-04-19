3bb1f5a364e044954ac85647422b41cb3edac90e
Trenbolone17 <albertsebastian2004@gmail.com>
2025-04-19 22:45:48 +0530

    added add pharmacy

--- procedures.sql
+++ procedures.sql
@@ -1,1 +1,6 @@
-
+delimiter //
+create procedure add_ph(in ph_name varchar(50), ph_add varchar(300), ph_contact varchar(20))
+begin
+INSERT INTO Pharmacy (ph_name, ph_add, ph_contact) VALUES
+(ph_name, ph_add, ph_contact);
+end //
